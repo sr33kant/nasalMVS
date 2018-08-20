3a6c229e2b059af5087b899fcf7a439ebb067bdb
sr33kant <sreekant.kondapaneni@hotmail.com>
2018-08-20 17:34:41 -0400

    Add files via upload

--- nosebleeds_zip_model.R
+++ nosebleeds_zip_model.R
@@ -2,7 +2,7 @@ require(ggplot2)
 require(pscl)
 require(MASS)
 require(boot)
-
+require(splines)
 
 efficacy_data<-read.csv("C:\\Users\\sreek\\Documents\\bayer\\Scenario\\efficacy.csv",header = T)
 subject_data<-read.csv("C:\\Users\\sreek\\Documents\\bayer\\Scenario\\subject.csv",header=T)
@@ -14,9 +14,9 @@ merged_data=merge(first_merge,randomized_data)
 ds<-merged_data[,c("nosebleeds","mucus.viscosity","tissue.use","country",'previous.year','arm')]
 bs$tissue.use=as.numeric(bs$tissue.use)
 bs$country=as.numeric(bs$country)
-summary( zingp<-zeroinfl(nosebleeds~.|.,data=ds,dist = "poisson"))
+summary( zingp<-zeroinfl(nosebleeds~bs(mucus.viscosity,degree=3)+ tissue.use+country+previous.year*arm|ns(mucus.viscosity,df=10)+ tissue.use+country+previous.year*arm,data=ds,dist = "poisson"))
 zingp$coefficients
-summary(p1 <- glm(nosebleeds ~ mucus.viscosity + tissue.use+country+previous.year+arm, family = poisson, data = ds))
+summary(p1 <- glm(nosebleeds ~ ns(mucus.viscosity,df=3)+ tissue.use+country+previous.year*arm, family = poisson, data = ds))
 summary.glm(p1)$dispersion
 vuong(p1,zingp)
 
@@ -29,3 +29,7 @@ pchisq(p1$deviance, df=p1$df.residual, lower.tail=FALSE)
 prob<-predict(zingp,type='zero')
 lamb_da<-predict(zingp,type='count')
 ifelse(rbinom(444, size = 1, prob = prob) > 0, 0, rpois(444, lambda = lamb_da))
+
+
+
+
