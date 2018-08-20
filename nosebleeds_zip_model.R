316253e7c9e2df3e3d263e41d9fbe3df043455c6
sr33kant <sreekant.kondapaneni@hotmail.com>
2018-08-19 21:09:05 -0400

    Add files via upload

--- nosebleeds_zip_model.R
+++ nosebleeds_zip_model.R
@@ -14,7 +14,7 @@ merged_data=merge(first_merge,randomized_data)
 ds<-merged_data[,c("nosebleeds","mucus.viscosity","tissue.use","country",'previous.year','arm')]
 bs$tissue.use=as.numeric(bs$tissue.use)
 bs$country=as.numeric(bs$country)
-summary( zingp<-zeroinfl(nosebleeds~.|.,data=ds,dist = "poisson"))
+summary( zingp<-zeroinfl(nosebleeds~mucus.viscosity|.,data=ds,dist = "poisson"))
 zingp$coefficients
 summary(p1 <- glm(nosebleeds ~ mucus.viscosity + tissue.use+country, family = poisson, data = ds))
 summary.glm(p1)$dispersion
@@ -28,3 +28,4 @@ pchisq(p1$deviance, df=p1$df.residual, lower.tail=FALSE)
 ### find p and lambda values 
 prob<-predict(zingp,type='zero')
 lamb_da<-predict(zingp,type='count')
+ifelse(rbinom(444, size = 1, prob = prob) > 0, 0, rpois(444, lambda = lamb_da))
